1ab157b73b11cd495dba079eb43beec9ea0a2ce5
Mike Mueller <mikemueller04@hotmail.com>
2016-03-16 16:35:20 -0400

    able to read text field

--- app/src/main/java/com/example/mike/droidevercraft/MainActivity.java
+++ app/src/main/java/com/example/mike/droidevercraft/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.mike.droidevercraft;
 import android.media.MediaMetadataRetriever;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         play = new Play();
-        everChar1 = new EverCraftCharacter("name1", Enum.Alignment.Good);
+
+
+    }
+
+    public void roll(View view){
+
+        EditText textInput = (EditText) findViewById(R.id.name_input);
+        Editable nameInput = textInput.getText();
+        String nameInputStr = nameInput.toString();
+        everChar1 = new EverCraftCharacter(nameInputStr, Enum.Alignment.Good);
         everChar2 = new EverCraftCharacter("name2", Enum.Alignment.Good);
         int hp = everChar1.getHitPoints();
         String hpStr = Integer.toString(hp);
@@ -28,5 +41,9 @@ public class MainActivity extends AppCompatActivity {
         hp = everChar1.getHitPoints();
         hpStr = Integer.toString(hp);
         Log.d("after", hpStr);
+        String nameStr = everChar1.getName();
+        Log.d("name", nameStr);
     }
+
+
 }
