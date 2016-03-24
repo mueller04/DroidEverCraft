dad845d00b01113083fb77d806d78362ad889c08
Mike Mueller <mikemueller04@hotmail.com>
2016-03-23 20:47:28 -0400

    updating race in spinner working

--- app/src/main/java/com/example/mike/droidevercraft/CharacterSetup.java
+++ app/src/main/java/com/example/mike/droidevercraft/CharacterSetup.java
@@ -9,17 +9,26 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.gson.Gson;
 
-public class CharacterSetup extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    //TODO this screen doesn't seem to handle the device back button at the bottom of the device
+public class CharacterSetup extends AppCompatActivity {
 
     Gson gs;
     EverCraftCharacter everChar;
     private EditText characterName;
+    private int raceEnumIndex;
+
+    private Spinner raceSpinner;
+
+    private ArrayList<EverEnum.RaceEnum> raceSpinnerOptions;
 
 
     @Override
@@ -36,6 +45,48 @@ public class CharacterSetup extends AppCompatActivity {
         everChar = gs.fromJson(evercharSerialized2, EverCraftCharacter.class);
         characterName = (EditText) findViewById(R.id.character_name_input);
         characterName.setText(everChar.getName());
+
+
+
+        createRaceSpinner();
+    }
+
+
+    private void createRaceSpinner(){
+        raceSpinnerOptions = new ArrayList<>();
+        int index = 0;
+        for (EverEnum.RaceEnum e : EverEnum.RaceEnum.values()){
+            raceSpinnerOptions.add(e);
+            if (e == everChar.getRace()){
+                raceEnumIndex = index;
+            }
+            index++;
+        }
+
+        raceSpinner = (Spinner)findViewById(R.id.race_spinner);
+        ArrayAdapter<EverEnum.RaceEnum>adapter = new ArrayAdapter<EverEnum.RaceEnum>(CharacterSetup.this,
+                android.R.layout.simple_spinner_item, raceSpinnerOptions);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        raceSpinner.setAdapter(adapter);
+        raceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                EverEnum.RaceEnum currentRaceSelection = (EverEnum.RaceEnum)parent.getItemAtPosition(position);
+                everChar.setRace(currentRaceSelection);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //we aren't even hitting this method, how is this called?  I want it to show the current rance when player hits screen
+
+
+            }
+        });
+
+        raceSpinner.setSelection(raceEnumIndex);
+        Log.d("raceEnumIndex", String.valueOf(raceEnumIndex));
     }
 
     //TODO I'm guessing this is for the navigation bar back button..doesn't fire or do anything
